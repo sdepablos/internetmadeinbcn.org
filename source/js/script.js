adbe8586afe3db4a0fea264685f4e5237e9f908c
Daniel Guillan <daniel.guillan@gmail.com>
2012-08-17 16:24:03 +0200

    Initial styling. Includes Sassbox, main structure and styles, wufoo form integration.

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,37 @@
-/* Author:
+$(function(){
+	$('.join').on('click','.js-btn-join',function(e){
+		e.preventDefault();
+		$btn = $(this)
+		$btn.toggleClass('active');
+		$('.apply').stop().slideToggle(460);
+		var currentScroll = $(window).scrollTop(),
+			btnPosition = $btn.offset().top,
+			distance = Math.abs(currentScroll - btnPosition),
+			speed = 1000,
+			time = (distance/speed)*1000;
 
-*/
+		$body.animate({scrollTop: btnPosition - 50}, time);
+
+	});
 
 
+	var $body = $('html, body');
 
+	$body.on('click','.js-smooth-scroll', function() {
+		smoothScroll($(this));
+		e.preventDefault();
+	});
 
+	function smoothScroll(anchor) {
+		var $anchor = $(anchor),
+			$el = $($anchor.attr('href')),
+			distance = Math.abs($el.offset().top - $anchor.offset().top),
+			speed = 2000,
+			time = (distance/speed)*1000,
+			customSpeed = $anchor.data('smoothspeed');
 
+		$body.animate({scrollTop: $el.offset().top}, customSpeed ? customSpeed : time);
+		return false;
+	}
+
+});
