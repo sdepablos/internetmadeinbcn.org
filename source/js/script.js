5ec7c2ae0e28284bd24326c4ba90cf797521c0fa
Daniel Guillan <daniel.guillan@gmail.com>
2012-08-18 23:48:23 +0200

    Javascript cleanup

--- source/js/script.js
+++ source/js/script.js
@@ -1,36 +1,22 @@
 $(function(){
 	$('.join').on('click','.js-btn-join',function(e){
+		// Dont' follow the link
 		e.preventDefault();
-		$btn = $(this)
-		$btn.toggleClass('active');
-		$('.apply').stop().slideToggle(460);
-		var currentScroll = $(window).scrollTop(),
+
+		// Scrolling vars
+		var $btn = $(this),
+			currentScroll = $(window).scrollTop(),
 			btnPosition = $btn.offset().top,
 			distance = Math.abs(currentScroll - btnPosition),
 			speed = 1000,
 			time = (distance/speed)*1000;
 
-		$body.animate({scrollTop: btnPosition - 50}, time);
+		// Toggle join form section display
+		$btn.toggleClass('active');
+		$('.apply').stop().slideToggle(460);
+
+		// Scroll to join form section
+		$('html, body').animate({scrollTop: btnPosition - 50}, time);
 
 	});
-
-	var $body = $('html, body');
-
-	$body.on('click','.js-smooth-scroll', function() {
-		smoothScroll($(this));
-		e.preventDefault();
-	});
-
-	function smoothScroll(anchor) {
-		var $anchor = $(anchor),
-			$el = $($anchor.attr('href')),
-			distance = Math.abs($el.offset().top - $anchor.offset().top),
-			speed = 2000,
-			time = (distance/speed)*1000,
-			customSpeed = $anchor.data('smoothspeed');
-
-		$body.animate({scrollTop: $el.offset().top}, customSpeed ? customSpeed : time);
-		return false;
-	}
-
 });
