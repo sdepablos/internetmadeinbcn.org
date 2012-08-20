78defa71acdd5c60a670123695b16c7bf5e3f27f
Daniel Guillan <daniel.guillan@gmail.com>
2012-08-20 14:12:10 +0200

    Catalan and spanish Localization

--- config.rb
+++ config.rb
@@ -3,6 +3,9 @@ set :css_dir, 'css'
 set :js_dir, 'js'
 set :images_dir, 'img'
 
+# Localization (i18n)
+activate :i18n, :path => "/:locale/", :mount_at_root => :es
+
 # Build-specific configuration
 configure :build do
   activate :minify_css
